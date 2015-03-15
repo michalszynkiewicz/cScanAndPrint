b9d34f522297c264247dc5c3ce637f8bafdea1b7
michalszynkiewicz <michal.l.szynkiewicz@gmail.com>
2015-03-15 13:16:32 +0100

    Update 5-getc-getchar-and-gets.c

--- 5-getc-getchar-and-gets.c
+++ 5-getc-getchar-and-gets.c
@@ -3,18 +3,18 @@
 int main() {
 	int c;
 	char buffer[4096];
-	printf("enter a char to read with getc: ");
-	c = getc(stdin); /* has the same effect as getchar() */
-	getchar(); /* for newline character ! */
-	printf("you entered %c\n", c);
+	printf("podaj znak do wczytania przez getc: ");
+	c = getc(stdin); /* to samo co getchar() */
+	getchar(); /* żeby wczytać znak końca linii ! */
+	printf("podany znak: '%c'\n", c);
 
-	printf("enter a string to read by gets: ");
+	printf("podaj ciąg znaków do wczytania przez gets: ");
 	gets(buffer);
-	printf("you entered: %s\n", buffer);
+	printf("podany ciąg znaków: '%s'\n", buffer);
 
-	printf("enter a string and note the newline char left by fgets: ");
+	printf("podaj ciąg znaków do wczytania przez fgets: ");
 	fgets(buffer, 4096, stdin);
-	printf("you entered: %s\n", buffer);
+	printf("Zwróć uwagę na znak końca linii. Podany ciąg znaków: '%s'\n", buffer);
 	
 	return 0;
 }
